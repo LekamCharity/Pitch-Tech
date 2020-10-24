2640cdde7d5b37e3d1decca1d05354506652bc57
LekamCharity.gmail.com <LekamCharity.gmail.com>
2020-10-24 22:40:04 +0300

    created an instance of app and installed config

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_bootstrap import Bootstrap
 from flask import Flask
 from flask_mail import Mail
 from flask_login import LoginManager
-from flask_uploads import IMAGES, UploadSet,configure_uploads
+from config import config_options
 
 
 bootstrap = Bootstrap()
@@ -12,7 +12,6 @@ mail = Mail()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
-photos = UploadSet('photos', IMAGES)
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -24,7 +23,6 @@ def create_app(config_name):
     app.register_blueprint(main_blueprint)
 
     login_manager.init_app(app)
-    configure_uploads(app, photos)
     mail.init_app(app)
     bootstrap.init_app(app)
     db.init_app(app)
--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from . import db,login_manager
+from flask_login import UserMixin
 
-class User(UserMixin,db.model):
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(255),unique = True,nullable = False)
@@ -12,7 +13,7 @@ class User(UserMixin,db.model):
     upvote = db.relationship('Upvote',backref='user',lazy='dynamic')
     downvote = db.relationship('Downvote',backref='user',lazy='dynamic') 
 
-     @property
+    @property
     def password(self):
         raise AttributeError('You cannot read the password attribute')
 
