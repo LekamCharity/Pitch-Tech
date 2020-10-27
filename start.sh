48b36d32e53080668e6a0c2a75409d2f3da241ef
LekamCharity.gmail.com <LekamCharity.gmail.com>
2020-10-27 21:36:10 +0300

    deployment to heroku

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 export DATABASE_URI ='postgresql+psycopg2://moringa:lekam2020@localhost/pitchtech'
 export SECRET_KEY='LEKAM'
-export MAIL_USERNAME='kerrykomar@gmail.com'
-
-export MAIL_PASSWORD='dellakomar'
+export MAIL_USERNAME='lekamcharity@gmail.com'
+export MAIL_PASSWORD='kulangombe'
 
 python3.8 manage.py server
