f0ab72dc2b00c2c56e4f22c064253ec015194db0
LekamCharity.gmail.com <LekamCharity.gmail.com>
2020-10-27 12:23:06 +0300

    refreshed the requirements

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask_mail import Mail
 from flask_login import LoginManager
 from config import config_options
+from flask_uploads import IMAGES, UploadSet,configure_uploads
 
 
 bootstrap = Bootstrap()
@@ -12,6 +13,7 @@ mail = Mail()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
+photos = UploadSet('photos',IMAGES)
 
 def create_app(config_name):
     app = Flask(__name__)
